6659114ad447aca7c889ae61280dbd57bdc21056
yeonjooRyu <yeonjoo3751@gmail.com>
2023-08-09 18:14:08 +0900

    HomeActivity 수정 commit

--- app/src/main/java/com/example/introduceapp/HomeActivity.kt
+++ app/src/main/java/com/example/introduceapp/HomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.example.introduceapp.R.id.btn_login
 
 class HomeActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val strData = intent.getStringExtra("dataFromSignInActivity")
-        val editText = findViewById<EditText>(R.id.editText_blank)
-        editText.setText("아이디: $strData")
+        val textView = findViewById<TextView>(R.id.editText_blank)
+        textView.setText("아이디: $strData")
 
         val btn = findViewById<Button>(R.id.btn_end)
         btn.setOnClickListener{
